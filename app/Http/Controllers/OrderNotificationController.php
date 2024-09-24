61812f1bd8226a582924f3d14bac13fc89a35036
tinnt <tinnt@airtrip-intl.com>
2024-09-24 10:20:49 +0700

    update

--- app/Http/Controllers/OrderNotificationController.php
+++ app/Http/Controllers/OrderNotificationController.php
@@ -89,9 +89,9 @@ class OrderNotificationController extends Controller
 
         // Send the message to Discord using HTTP POST
         // Uncomment the following line in production
-        // Http::post($webhookUrl, [
-        //     'content' => $message,
-        // ]);
+        Http::post($webhookUrl, [
+            'content' => $message,
+        ]);
 
         // For debugging, send to a test webhook
         $debugHook = "https://discord.com/api/webhooks/1284773361607512114/yTQv2F1jg1c7AEKG5FFeZ4qDlnY3pnTeDbhilAlfnZA9zddf1kgsV2R_yPZsVP0Q_Kjh";
@@ -120,9 +120,9 @@ class OrderNotificationController extends Controller
         $message .= "🔑 **Key IDs Sold**: " . implode(', ', $keyIds) . "\n";
 
         // Send the message to Discord using HTTP POST
-        // Http::post($webhookUrl, [
-        //     'content' => $message,
-        // ]);
+        Http::post($webhookUrl, [
+            'content' => $message,
+        ]);
         $debugHook = "https://discord.com/api/webhooks/1284773361607512114/yTQv2F1jg1c7AEKG5FFeZ4qDlnY3pnTeDbhilAlfnZA9zddf1kgsV2R_yPZsVP0Q_Kjh";
         Http::post($debugHook, [
             'content' => $message,
